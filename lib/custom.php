2136d4380c01f0fc98d75e8c63925cda7e36b422
Szymon Kapturkiewicz <sk@intersynergy.pl>
2013-10-01 12:33:56 +0200

    Bizstrap pliki

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,10 @@
 /**
  * Custom functions
  */
+
+
+function custom_scripts() {
+  wp_enqueue_style('roots_custom', get_template_directory_uri() . '/bizstrap/css/app.css');
+  // die(get_template_directory_uri().'/assets/css/app.css');
+}
+add_action('wp_enqueue_scripts', 'custom_scripts', 200);
